8cfffb19a31739fca7adc381f30c526ed21ecbd3
Jordan Abreu <jabreu610@gmail.com>
2025-12-02 22:21:12 -0500

    ✨ Initialize placeholder command

--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
-console.log("Hello via Bun!");
-console.log(Bun.argv);
+import createCli from "cac";
+import { $ } from "bun";
+
+const cli = createCli("gwt");
+cli
+  .command("ls", "placeholder command, lists current working directory files")
+  .action(async () => {
+    await $`ls`;
+  });
+
+cli.help();
+cli.version("0.0.0");
+cli.parse();
